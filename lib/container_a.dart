7914ee49e3630a8a8976fa7af7adf96c067b73a7
think <350592146@qq.com>
2019-06-05 17:04:07 +0800

    容器示例

--- lib/container_a.dart
+++ lib/container_a.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: Text('Flutter Demo'),
+            title: Text('Flutter Container 组件'),
           ),
           body: MyBody()
       ),
