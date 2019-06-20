9fca08c2163fbe4935f7ba25c59a74ac4c46b876
think <350592146@qq.com>
2019-06-20 10:30:12 +0800

    通过TabController 定义TabBar

---
+++ lib/appBarTabPages/TabControllerPage.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+///通过TabController 定义TabBar
+///
+
+class TabControllerPage extends StatefulWidget {
+  @override
+  _TabControllerPageState createState() => _TabControllerPageState();
+}
+
+class _TabControllerPageState extends State<TabControllerPage> with SingleTickerProviderStateMixin{
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    print('初始化 数据...');
+    _tabController = new TabController(
+      vsync: this,//固定写法
+      length: 9   //指定tab长度
+    );
+    //添加监听
+    _tabController.addListener((){
+      var index = _tabController.index;
+      var previousIndex = _tabController.previousIndex;
+      print("index: $index");
+      print('previousIndex: $previousIndex');
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('使用TabController完成TabBar'),
+          bottom: TabBar(
+            controller: _tabController,
+            isScrollable: true,
+            tabs: <Widget>[
+              Tab(text: '推荐',),
+                Tab(text: '丽人',),
+                Tab(text: '旅行',),
+                Tab(text: '电影',),
+                Tab(text: '结婚',),
+                Tab(text: '购物',),
+                Tab(text: '教培',),
+                Tab(text: '家装',),
+                Tab(text: '亲子',),
+            ],
+          ),
+        ),
+
+        body: TabBarView(
+          controller: _tabController,
+          children: <Widget>[
+            Container(
+              color: Color(0xffffffff),
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.all(20),
+              child: Text('TabPage 1',style: TextStyle(fontSize: 30),),
+            ),
+            Container(
+              color: Color(0xffffffff),
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.all(20),
+              child: Text('TabPage 2',style: TextStyle(fontSize: 30),),
+            ),
+            Container(
+              color: Color(0xffffffff),
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.all(20),
+              child: Text('TabPage 3',style: TextStyle(fontSize: 30),),
+            ),
+            Container(
+              color: Color(0xffffffff),
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.all(20),
+              child: Text('TabPage 4',style: TextStyle(fontSize: 30),),
+            ),
+            Container(
+              color: Color(0xffffffff),
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.all(20),
+              child: Text('TabPage 5',style: TextStyle(fontSize: 30),),
+            ),
+            Container(
+              color: Color(0xffffffff),
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.all(20),
+              child: Text('TabPage 6',style: TextStyle(fontSize: 30),),
+            ),
+            Container(
+              color: Color(0xffffffff),
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.all(20),
+              child: Text('TabPage 7',style: TextStyle(fontSize: 30),),
+            ),
+            Container(
+              color: Color(0xffffffff),
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.all(20),
+              child: Text('TabPage 8',style: TextStyle(fontSize: 30),),
+            ),
+            Container(
+              color: Color(0xffffffff),
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.all(20),
+              child: Text('TabPage 9',style: TextStyle(fontSize: 30),),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
---
+++ lib/tools/fluttertoast.dart
@@ -0,0 +1,103 @@
+import 'dart:async';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:meta/meta.dart';
+import 'package:flutter/foundation.dart';
+
+enum Toast { LENGTH_SHORT, LENGTH_LONG }
+
+enum ToastGravity { TOP, BOTTOM, CENTER }
+
+class Fluttertoast {
+  static const MethodChannel _channel =
+  const MethodChannel('PonnamKarthik/fluttertoast');
+
+  // for Version 4.x.x
+
+  // static Fluttertoast _instance;
+
+  // static Fluttertoast get instance {
+  //   if (_instance == null) {
+  //     _instance =Fluttertoast._create();
+  //   }
+  //   return _instance;
+  // }
+
+  // Fluttertoast._create(){
+  // }
+
+  static Future<bool> cancel() async {
+    bool res = await _channel.invokeMethod("cancel");
+    return res;
+  }
+
+
+  static Future<bool> showToast({
+    @required String msg,
+    Toast toastLength,
+    int timeInSecForIos = 1,
+    double fontSize = 16.0,
+    ToastGravity gravity,
+    Color backgroundColor,
+    Color textColor,
+    // Function(bool) didTap,
+  }) async {
+    // this.didTap = didTap;
+    String toast = "short";
+    if (toastLength == Toast.LENGTH_LONG) {
+      toast = "long";
+    }
+
+    String gravityToast = "bottom";
+    if (gravity == ToastGravity.TOP) {
+      gravityToast = "top";
+    } else if (gravity == ToastGravity.CENTER) {
+      gravityToast = "center";
+    } else {
+      gravityToast = "bottom";
+    }
+
+    if(backgroundColor == null && defaultTargetPlatform == TargetPlatform.iOS) {
+      backgroundColor = Colors.black;
+    }
+    if(textColor == null && defaultTargetPlatform == TargetPlatform.iOS) {
+      textColor = Colors.white;
+    }
+    final Map<String, dynamic> params = <String, dynamic>{
+      'msg': msg,
+      'length': toast,
+      'time': timeInSecForIos,
+      'gravity': gravityToast,
+      'bgcolor': backgroundColor != null ? backgroundColor.value : null,
+      'textcolor': textColor != null ? textColor.value: null,
+      'fontSize': fontSize,
+    };
+
+    bool res = await _channel.invokeMethod('showToast', params);
+    return res;
+  }
+
+}
+/*
+
+Fluttertoast.showToast(
+        msg: "This is Center Short Toast",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIos: 1,
+        backgroundColor: Color(0xffe74c3c),
+        textColor: Color(0xffffffff),
+    );
+
+
+
+Fluttertoast.showToast(
+      msg: "网络连接错误",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIos: 1,
+      bgcolor: "#9E9E9E",
+      textcolor: '#ffffff');
+}
+ */
